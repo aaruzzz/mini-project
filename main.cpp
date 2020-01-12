336db67f3dff149e22fd04ab1afddd028884c005
buddha69 <buddhagautam231gmail.com>
2020-01-12 11:34:12 +0545

    finalize

--- main.cpp
+++ main.cpp
@@ -4,11 +4,11 @@
 using namespace std;
 
 
-int pr(char a){
+int pr(char a){                 //function to check priority
 	
-	if (a=='+') return 1;
+	if (a=='+') return 1; //lower and same precedence
 	if (a=='-') return 1;
-	if (a=='/') return 2;
+	if (a=='/') return 2; //higher and same precedence
 	if (a=='*') return 2;
 }
 
@@ -17,68 +17,57 @@ int main()
 Stack s;
 char infix[40];
 char postfix[40];
-int count=0;
-int pf=0;
-char fetch;
-int invalidCheck=0;
+int count=0;                        //index for infix expression array
+int pf=0;			    //index of postfix expression array
+char fetch;			    //seperating each char of infix expression
+int invalidCheck=0;		    //to keep check if the expression is invalid so postfix 
 
 scanf("%s",infix);
 
-
-//scanf("%s",postfix);
-
 while(count<strlen(infix)){
 	char fetch= infix[count];
-		if(fetch=='+'||fetch=='-'||fetch=='/'||fetch=='*')
+		if(fetch=='+'||fetch=='-'||fetch=='/'||fetch=='*')   
 		{
-			if(s.isEmpty()) {s.push(fetch) ;} //if stack is empty 
-			else if(s.tops()=='(') {s.push(fetch);}//if previous element in stact is (
-			else if(pr(fetch)>pr(s.tops())){ s.push(fetch);}  //priority check
+			if(s.isEmpty()) {s.push(fetch) ;}                      //if stack is empty 
+			else if(s.tops()=='(') {s.push(fetch);}               //if previous element in stact is (
+			else if(pr(fetch)>pr(s.tops())){ s.push(fetch);}     //priority check
 			else 
 			{
-				
-				
 				postfix[pf++]= s.pop();
 				s.push(fetch);
-				
-				
 			}
 		}
-		else if(fetch == '(') {s.push(fetch);} //push opening brackets
-			
 		
+		else if(fetch == '(') {s.push(fetch);} //push opening brackets
+				
 		else if (fetch == ')'){
 			char z='0' ;
 			do
 			{	
-				
-				if(!s.isEmpty()) z=s.pop();
-				
-					if(s.isEmpty()) {cout<<"more closing brackets";invalidCheck=1;break;}
-					else postfix[pf++]=z; 
+				if(!s.isEmpty()) z=s.pop();        //to avoid popping from empty stack
+				if(z!='('){
+					if(s.isEmpty()) 
+					{
+						cout<<"more closing brackets";
+						invalidCheck=1;
+						break;
+					}
+					else postfix[pf++]=z;
 				}
 				
-				//else printf("%c",z);
-				//else {printf("%c",c);break;}
+				
 			}while(z!='(');
-			
+		}
 				
-				
-				
-			}
-			
-		
-		
-		else postfix[pf++]=fetch;//printf("%c3",fetch);
+		else postfix[pf++]=fetch;		//for operands
 count++;
-
 }
 
 while(!s.isEmpty() && !invalidCheck){
 	char p=s.pop();
-	if(p=='(') 
+	if(p=='(') 			//if open brackets left in the stack than invalid
 	{
-		cout <<"more opening brackets"<<endl;
+		cout <<"more opening brackets"<<endl;  
 		invalidCheck=1;
 		break;
 	}
@@ -86,8 +75,8 @@ while(!s.isEmpty() && !invalidCheck){
 	
 }
 
-postfix[pf]='\0';
-if(!invalidCheck) puts(postfix);
+postfix[pf]='\0';			   //making the array a string
+if(!invalidCheck) puts(postfix);          //if the expression isn't invalid print postfix expression
 
 return 0;
 }
